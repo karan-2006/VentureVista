e4ed7c69ca3f8c8d498083ba82fe82ff8e90a445
karan-2006 <karansunil2006@gmail.com>
2025-04-23 13:01:26 +0530

    Implement password reset functionality with email backend and templates

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
@@ -6,6 +7,10 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),name='password_reset_complete'),
     path("confirm-offer-booking/<int:offer_id>/", views.confirm_offer_booking, name="confirm_offer_booking"),
     path('offer-booking-success/<int:offer_id>/', views.offer_booking_success, name='offer_booking_success'),
     path("profile/", views.profile, name="profile"),
